1beb991bd741faf454fb4741e620e59ab37673ca
uko <iamdariusw@gmail.com>
2022-05-13 10:47:29 -0400

    changing menu & catching invalid selections

--- src/com/darius/wtf/Main.java
+++ src/com/darius/wtf/Main.java
@@ -1,25 +1,64 @@
 package com.darius.wtf;
 
-import java.util.Locale;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Random rand = new Random();
+
 
         // difficulty selection
-        System.out.println("Select your difficulty! (Easy, Medium, Hard, Random)");
-        String difficulty = scanner.nextLine().toLowerCase(Locale.ROOT);
-        if (difficulty.equals("easy")) Game.easyGame();
-        if (difficulty.equals("medium"))Game.medGame();
-        if (difficulty.equals("hard"))Game.hardGame();
-        if (difficulty.equals("random")) {
-            int randGameNum = rand.nextInt(100000);
-            Game.randGame(randGameNum);
-            scanner.close();
+
+
+        //menu
+        while (true) {
+            int difficulty = 0;
+            System.out.println("""
+                    Select your difficulty!
+                    1.Easy
+                    2.Medium
+                    3.Hard
+                    4.Random
+                    9.Exit
+                    """);
+            Scanner scanner = new Scanner(System.in);
+        try {
+            //catching an invalid menu option
+            difficulty = scanner.nextInt();
+            } catch (NoSuchElementException e) {
+            System.out.println("Please enter a number!");
+            }
+            //game difficulties
+            switch (difficulty) {
+                case 1 -> {
+                    scanner.nextLine();
+                    Game.easyGame();
+                }
+                case 2 -> {
+                    scanner.nextLine();
+                    Game.medGame();
+                }
+                case 3 -> {
+                    scanner.nextLine();
+                    Game.hardGame();
+                }
+                case 4 -> {
+                    scanner.nextLine();
+                    Random rand = new Random();
+                    int randGameNum = rand.nextInt(100000);
+                    Game.randGame(randGameNum);
+                }
+                case 9 -> {
+                    System.out.println("Goodbye!");
+                    scanner.close();
+                    System.exit(0);
+                }
+                default -> {
+                    System.out.println();
+                    scanner.nextLine();
+                }
+            }
+
         }
     }
 }

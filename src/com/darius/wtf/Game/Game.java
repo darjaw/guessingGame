505a8d274c0e49589267bdfca3819721f164f97d
uko <iamdariusw@gmail.com>
2022-05-14 17:44:18 -0400

    code refactor

--- src/com/darius/wtf/Game/Game.java
+++ src/com/darius/wtf/Game/Game.java
@@ -1,29 +1,14 @@
 package com.darius.wtf.Game;
 
-import java.util.Random;
 import java.util.Scanner;
 
 abstract class Game {
 
     private final Scanner scanner = new Scanner(System.in);
-    private int score = 0;
-    private int guess;
 
-//getters and setters
+    //getters and setters
     public int getScore() {
-        return score;
-    }
-
-    public void setScore(int score) {
-        this.score = score;
-    }
-
-    public int getGuess() {
-        return guess;
-    }
-
-    public void setGuess(int guess) {
-        this.guess = guess;
+        return 0;
     }
 
     public Scanner getScanner() {
